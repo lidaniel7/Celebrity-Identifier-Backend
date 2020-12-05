d8cc099b626ba99c125ded008a001adebf9b996f
Daniel Li <lidaniel225@gmail.com>
2020-12-05 15:49:11 -0800

    adding backend to face recognition app

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register.js')
+const signin = require('./controllers/signin.js')
+const image = require('./controllers/image');
+const profile = require('./controllers/profile.js');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'lidaniel',
+        password: '',
+        database: 'smart-brain'
+    }
+});
+
+db.select('*').from('users').then(data => { //don't need to convert to json because not http request
+    console.log(data)
+})
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.send('success')
+})
+
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db)})
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt, saltRounds)})
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt, saltRounds)})
+
+app.post('/imageurl', (req, res) => { image.handleAPICall(req, res) })
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000')
+})
